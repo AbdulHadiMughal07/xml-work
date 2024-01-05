806b342f07615da3959634bf8365c936801439ed
Hafiz Abdul Hadi Mughal <hadiaptech81@gmail.com>
2024-01-05 23:13:24 +0500

    Compare Products Pracice

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ for(let h=1;h<products.length;h++){
     <option value="${h}">${products[h].title}</option>
     `
 }
+
+function item1(h){
+    document.getElementById('img1').src = products[h].image
+    document.getElementById('price1').innerHTML = products[h].price
+    document.getElementById('brand1').innerHTML = products[h].brand
+    document.getElementById('des1').innerHTML = products[h].description
+}
+
+function item2(h){
+    document.getElementById('img2').src = products[h].image
+    document.getElementById('price2').innerHTML = products[h].price
+    document.getElementById('brand2').innerHTML = products[h].brand
+    document.getElementById('des2').innerHTML = products[h].description
+}
